bab9e29a946359eda976d7d69160a8e05d5a297d
Rick Brown <ricky.brown.00@gmail.com>
2020-11-30 23:28:24 +0000

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Rick Brown on 30/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
