1a6dd47ed9388e06b6ce605c84ce13050efc9c12
Rick Brown <ricky.brown.00@gmail.com>
2020-12-01 00:41:14 +0000

    demonstrates modifiers and views

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,15 +7,93 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct BorderedCaption: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .font(.title)
+      .padding(10)
+      .overlay(
+        RoundedRectangle(cornerRadius: 15)
+          .stroke(lineWidth: 2)
+      )
+      .foregroundColor(Color.blue)
+  }
+}
+
+extension View {
+  func titleStyle() -> some View {
+    self.modifier(BorderedCaption())
+  }
+}
+
+struct Watermark: ViewModifier {
+  var text: String
+  
+  func body(content: Content) -> some View {
+    ZStack(alignment: .bottomTrailing) {
+      content
+      
+      Text(text)
+        .font(.caption)
+        .foregroundColor(.white)
+        .padding(5)
+        .background(Color.black)
     }
+  }
+}
+
+extension View {
+  func watermarked(with text: String) -> some View {
+    self.modifier(Watermark(text: text))
+  }
+}
+
+struct GridStack<Content: View>: View {
+  let rows: Int
+  let columns: Int
+  let content: (Int, Int) -> Content
+  
+  var body: some View {
+    VStack {
+      ForEach(0..<rows, id: \.self) { row in
+        HStack {
+          ForEach(0..<self.columns, id: \.self) { column in
+            self.content(row, column)
+          }
+        }
+      }
+    }
+  }
+  
+  init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+    self.rows = rows
+    self.columns = columns
+    self.content = content
+  }
+}
+
+
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Text("Hello, world!")
+        .titleStyle()
+      
+      Color.blue
+        .frame(width: 300, height: 300, alignment: .center)
+        .watermarked(with: "Rick Brown")
+      
+      GridStack(rows: 4, columns: 4) { row, col in
+        Image(systemName: "\(row * 4 + col).circle")
+        Text("R\(row) C\(col)")
+      }
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
